99f0b08d09e85eb3d35f247dbdd80da7dbe6f659
Tariq Siddique <tariq.siddique@xislabs.com>
2026-01-06 12:03:51 +0500

    client changes are done

--- Archive (4)/cryo-calculator-dev-files/constant.js
+++ Archive (4)/cryo-calculator-dev-files/constant.js
@@ -1,1 +1,1 @@
-const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbwS1u8xCPIk5_o4RFYxbKekU-ZzPD9GuSjANnziIV32rsKxArCQMMsZ8_7pudLVPs0X/exec";
+const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbyTMm85HJXTTz4S8crTn2GCS7Mw4Fjfusoz1RTh1q2LQAPNwmQBEiE4ojK9WdOXw159/exec";
