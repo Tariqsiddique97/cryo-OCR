6958f78ef9a74339dd3ca3f8a0c52802de4bb9ce
Tariq Siddique <tariq.siddique@xislabs.com>
2026-01-08 11:03:45 +0500

    reset password completed

--- Archive (4)/cryo-calculator-dev-files/constant.js
+++ Archive (4)/cryo-calculator-dev-files/constant.js
@@ -1,1 +1,1 @@
-const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbz71EeDIfIpZqu3rlQ_g8Lgo49oUVNiPUN7U_GQgxvUGNSSB-7bQ7sJXUvBb-iKEu8B/exec";
+const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbxj_b7DhOe--WxQxdnDwiNSwFfey7Pq2AE4ki4FH9kDe21I6GUQ_rzZb1XSd6-FHSsW/exec";
