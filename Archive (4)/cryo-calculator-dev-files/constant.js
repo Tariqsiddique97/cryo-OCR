c24ea2aaa70692686cd50ca1460f54103128edcc
Tariq Siddique <tariq.siddique@xislabs.com>
2026-01-14 12:47:58 +0500

    disclaimer added

--- Archive (4)/cryo-calculator-dev-files/constant.js
+++ Archive (4)/cryo-calculator-dev-files/constant.js
@@ -1,1 +1,17 @@
+/*
+ * © 2025 RMC Squared Inc. All Rights Reserved.
+ *
+ * The Cryo Tank Calculator, including its source code, algorithms, and interface design,
+ * is proprietary and confidential to RMC Squared Inc. Unauthorized reproduction,
+ * distribution, modification, or reverse engineering of this application or its underlying
+ * logic is strictly prohibited and protected under U.S. and international copyright and
+ * trade secret laws.
+ *
+ * © 2025 RMC Squared Inc. All Rights Reserved.
+ * Cryo Tank Calculator source code registered with the U.S. Copyright Office,
+ * Case No. 1-15021517871 (filed October 15, 2025).
+ *
+ * For licensed use or integration inquiries, contact chrismcbrown@gmail.com
+ */
+
 const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbwrb3d-OvJUtUCkvMMnwlFRnFeeE4EQUcFMYykUewWpme8f_0lTSs6DteWwGelBPbGD/exec";
